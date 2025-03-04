dde10f6ba68a7aa1d5368436f5421b867a981dee
namnd <namnguyennd7428@gmail.com>
2025-03-04 19:51:48 +0700

    add new author mutation graph ql

--- src/main/java/com/graphql/server/controller/BookController.java
+++ src/main/java/com/graphql/server/controller/BookController.java
@@ -3,6 +3,7 @@ package com.graphql.server.controller;
 import com.graphql.server.data.Author;
 import com.graphql.server.data.Book;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -17,5 +18,17 @@ public class BookController {
     @SchemaMapping
     public Author author(Book book) {
         return Author.getById(book.authorId());
+    }
+
+    @MutationMapping
+    public Author addNewAuthor(
+            @Argument String id,
+            @Argument String firstName,
+            @Argument String lastName
+    ) {
+        Author newAuthor = new Author(id, firstName, lastName);
+        System.out.println(newAuthor);
+
+        return newAuthor;
     }
 }
